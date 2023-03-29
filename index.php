4877b7046800c22f6852ae90c3564bf617d604ed
phrayse <80496902+phrayse@users.noreply.github.com>
2023-03-29 14:30:19 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Job Search index page" />
+	<meta name="keywords"    content="jobsearch, job, search" />
+	<meta name="author"      content="STUID, NAME" />
+	<!--link href="css/style.css" rel="stylesheet"/-->
+	<title>JobSearch Index</title>
+</head>
+<body>
+    <p>
+        NAME, STUID@student.swin.edu.au
+        <br>
+        I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source.
+    </p>
+
+    <p>
+        <a href="postjobform.php">Post a job vacancy</a>
+        <br>
+        <a href="searchjobform.php">Search for a job vacancy</a>
+        <br>
+        <a href="about.php">About this assignment</a>
+    </p>
+</body>
+</html>
