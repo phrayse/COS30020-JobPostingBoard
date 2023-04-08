959c78597564a5450d1e48943e71aa9fa4a7711f
phrayse <80496902+phrayse@users.noreply.github.com>
2023-04-08 18:31:46 +1000

    restructured and regex tested

--- postjobprocess.php
+++ postjobprocess.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="description" content="Job Post processing page" />
-  <meta name="keywords"    content="jobpost, job, search" />
+  <meta name="keywords"    content="post, processing" />
   <meta name="author"      content="STUID, NAME" />
   <!--link href="css/style.css" rel="stylesheet"/-->
   <title>PROCESSING</title>
@@ -11,39 +11,44 @@
 <body>
   <?php
   // This page checks the input data, writes the data to a text file, and generates the corresponding HTML output.
-  /* Task 3 Requirements:
-  Req 1:
-  1a) All fields must be supplied and valid for the PHP page to allow saving of the job vacancy to a text file.
-  1b) Date must also be validated to conform to the dd/mm/yy format.
-  1c) Position ID must be checked for uniqueness within the text file.
-  Req 2:
-  creation of the "jobposts" directory on Mercury server to store the job vacancy text file is automatically handled by the PHP script, if it does not exist.
-  Req 3:
-  Each vacancy should be saved in jobs.txt - each record should be one line, with a \n for each new line.
-  Each field in a record should be separated by a tab (\t).
-  The accept application by is stored as two separate columns.
-  Req 4:
-  Confirmation message should be generated for this entry followed by a link to return to the home page once the vacancy is stored successfully in the file.
-  */
+  print_r(array_filter($_POST));
   
-  // this line should ensure that every field has a response, but there might be a problem with the checkboxes idk yet
-  if (count(array_filter($_POST)) != count($_POST)) {
+  // Check the checkboxes are checked.
+  if (isset($_POST["postal"])) {
+    $applicationByPost = $_POST["postal"];
+  } else {
+    $applicationByPost = FALSE;
+  }
+  
+  if (isset($_POST["email"])) {
+    $applicationByEmail = $_POST["email"];
+  } else {
+    $applicationByEmail = FALSE;
+  }
+  
+  if (($applicationByPost == FALSE) && ($applicationByEmail == FALSE)) {
+    echo "<h1><strong>Cooked!</strong></h1>";
+    echo "<p>Please ensure an application method has been selected.";
+    echo "<br><a href=\"postjobform.php\">Try again</a></p>;
+  } else {
+    // initialise variables from form.
     $posID = $_POST["posID"];
     $title = $_POST["title"];
-    $description = $_POST["description"];
+    $description = htmlspecialchars($_POST["description"]);
     $closeDate = $_POST["closeDate"];
     $positionType = $_POST["positionType"];
     $contractType = $_POST["contractType"];
     $location = $_POST["location"];
-    // probs have to fuck around with these two to get them right
-    $applicationByPost = $_POST["postal"];
-    $applicationByEmail = $_POST["email"];
-
+    
+    // Call validate() to make sure every field meets criteria.
     $valid = validate($posID, $title, $description, $closeDate, $positionType, $contractType, $location, $applicationByPost, $applicationByEmail);
-    if ($valid == TRUE) {
-      echo "valid is true remove before submission";
+    if ($valid == FALSE) {
+      echo "valid is false";
     } else {
-      echo "valid is false remove before submission";
+      echo "valid is true";
+      
+      // rest of the program goes here.
+      // it'll be the filewriting stuff plus checks for unique posID.
   }
 
   // Validate function to make the main body cleaner looking.
@@ -71,10 +76,10 @@
     if ($contractType != NULL) {
       $matchCounter++;
     }
-    if ($location != ("blank" || NULL)) {
+    if ($location != (("" || NULL))) {
       $matchCounter++;
     }
-    if (($matchCounter == 7) && (($applicationByPost != NULL) || ($applicationByEmail != NULL))) {
+    if (($matchCounter == 7) && (($applicationByPost == TRUE) || ($applicationByEmail == TRUE))) {
       return true;
     } else {
       return false;
@@ -82,5 +87,23 @@
   }
 
   ?>
+  
+  <?php
+  /* Task 3 Requirements:
+  Req 1:
+  1a) All fields must be supplied and valid for the PHP page to allow saving of the job vacancy to a text file.
+  1b) Date must also be validated to conform to the dd/mm/yy format.
+  1c) Position ID must be checked for uniqueness within the text file.
+  Req 2:
+  creation of the "jobposts" directory on Mercury server to store the job vacancy text file is automatically handled by the PHP script, if it does not exist.
+  Req 3:
+  Each vacancy should be saved in jobs.txt - each record should be one line, with a \n for each new line.
+  Each field in a record should be separated by a tab (\t).
+  The accept application by is stored as two separate columns.
+  Req 4:
+  Confirmation message should be generated for this entry followed by a link to return to the home page once the vacancy is stored successfully in the file.
+  */
+  ?>
+  
 </body>
 </html>
