d4b6a5a1ce00885d8b2986a0db1eea0f9b832a03
phrayse <80496902+phrayse@users.noreply.github.com>
2023-04-08 17:56:24 +1000

    regex and required tags

--- postjobform.php
+++ postjobform.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="description" content="Job Search Post page" />
-	<meta name="keywords"    content="post, job, search" />
+	<meta name="keywords"    content="post, job, form" />
   <!-- Make sure to update stuid and name before submission --> 
 	<meta name="author"      content="STUID, NAME" />
 	<!--link href="css/style.css" rel="stylesheet"/-->
@@ -18,18 +18,18 @@
 				<!-- Position ID. Text input type, NOT NULL, should be unique. 5 characters, P and four numbers -->
 				<!-- Should this not be automatically set by server, not user? That way can autoassign unique ID -->
 				<p><label>Position ID:
-					<input id="posID" type="text" name="posID" pattern="/^[P]\d\d\d\d$/"/>
+					<input id="posID" type="text" name="posID" pattern="^P\d{4}$" required/>
 				</label></p>
 
 				<!-- Title. Text input type, NOT NULL. Max 20 alphanumeric incl. [ ,.!] -->
 				<p><label>Title:
-					<input id="title" type="text" name="title" pattern="/^[a-zA-Z0-9]+[a-zA-Z0-9,.! ]{1,20}$/"/>
+					<input id="title" type="text" name="title" pattern="^([a-zA-Z0-9][a-zA-Z0-9,.! ]*){1,20}$" required/>
 				</label></p>
 
 				<!-- Description. Text area type, NOT NULL. Max 260 characters -->
 				<!-- should I implement scrubbing here to avoid potential injection -->
 				<p><label for="description">Description:</label>
-					<textarea id="description" name="description" rows="4" cols="50" minlength="1" maxlength="260"></textarea>
+					<textarea id="description" name="description" rows="4" cols="50" minlength="1" maxlength="260" required></textarea>
 				</p>
 
 				<!-- Closing date. Text input type, NOT NULL, initial value is server's current date in dd/mm/yy -->
@@ -37,35 +37,33 @@
 				<!-- split the regex into a separate line cause it's very likely problematic -->
 				<p><label>Closing Date:
 					<?php
-						echo "<input id=\"closeDate\" type=\"text\" name=\"closeDate\" value=\"date(\"d-m-y\")\"";
-						echo " pattern=\"/^\\d{1,2}\\/\\d{1,2}\\/\\d{2}$/\\" />";
+						$date = date("d/m/y");
+						echo "<input id=\"closeDate\" type=\"text\" name=\"closeDate\" value=$date pattern=\"^\d{1,2}/\d{1,2}/\d{2}$\" required />";
 					?>
 				</label></p>
 
 				<!-- Position. Radio button type, 2 options -->
-				<!-- Need to add some sort of validation here to ensure one option has been selected -->
 				<p>Position:
-					<label><input id="fullTime" type="radio" name="positionType" value="fullTime" />Full Time</label>
+					<label><input id="fullTime" type="radio" name="positionType" value="fullTime" required />Full Time</label>
 					<label><input id="partTime" type="radio" name="positionType" value="partTime" />Part Time</label>
 				</p>
 
 				<!-- Contract. Radio button type, 2 options -->
-				<!-- Need to add some sort of validation here to ensure one option has been selected -->
 				<p>Contract:
-					<label><input id="ongoing" type="radio" name="contractType" value="ongoing" />On-going</label>
+					<label><input id="ongoing" type="radio" name="contractType" value="ongoing" required />On-going</label>
 					<label><input id="fixedTerm" type="radio" name="contractType" value="fixedTerm" />Fixed Term</label>
 				</p>
 
 				<!-- Accept application by. Checkbox input type, 2 options -->
 				<p>Application by:
-					<label><input id="postal" type="checkbox" name="applicationType" value="postal" />Post</label>
-					<label><input id="email" type="checkbox" name="applicationType" value="email" />Email</label>
+					<label><input id="postal" type="checkbox" name="postal" value=TRUE />Post</label>
+					<label><input id="email" type="checkbox" name="email" value=TRUE />Email</label>
 				</p>
 
 				<!-- Location. Select option type, initial value "---" with options for each state -->
 				<p><label for="location">Location:</label>
-					<select id="location" name="location">
-						<option value="blank" selected disabled hidden>---</option>
+					<select id="location" name="location" required>
+						<option value="" selected disabled hidden>---</option>
 						<option value="ACT">ACT</option>
 						<option value="NSW">NSW</option>
 						<option value="NT">NT</option>
