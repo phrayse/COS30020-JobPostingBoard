5f78e85c5c0c2d20d738db72e7376909ebec7338
phrayse <80496902+phrayse@users.noreply.github.com>
2023-04-25 13:20:59 +1000

    Add broken search filter
    It might be working now who knows.
    checkFilter() is unnecessary, it should be integrated with applyFilter() to cut down on creating unnecessary variables ($filter doesn't need the "00;12; type debug code)

--- searchjobprocess.php
+++ searchjobprocess.php
@@ -27,6 +27,11 @@
     	}
 			echo "<h1>Search results</h1>";
 			if (! $flagged == "") {
+				$filter = "";
+				$filter = checkFilter($filter);
+				if (! $filter == "") {
+					$flagged = applyFilter($flagged, $filter, $search);
+				}
 				printflagged($flagged);
 			} else {
 				echo "<p><em>No search results</em>";
@@ -43,6 +48,155 @@
 		echo "<br><a href=\"searchjobform\">Back to search</a></p>";
   }
 
+	// This is unnecessary - should be integrated into applyFilter().
+	function checkFilter($filter) {
+    if ($_GET["fieldFilter"] == "any") {$filter .= "00;";}
+    if ($_GET["fieldFilter"] == "posID") {$filter .= "01;";}
+    if ($_GET["fieldFilter"] == "title") {$filter .= "02;";}
+
+    if ($_GET["posFilter"] == "any") {$filter .= "10;";}
+    if ($_GET["posFilter"] == "fTime") {$filter .= "11;";}
+    if ($_GET["posFilter"] == "pTime") {$filter .= "12;";}
+
+    if ($_GET["conFilter"] == "any") {$filter .= "20;";}
+    if ($_GET["conFilter"] == "ongoing") {$filter .= "21;";}
+    if ($_GET["conFilter"] == "fixedTerm") {$filter .= "22;";}
+
+    if ($_GET["appFilter"] == "any") {$filter .= "30;";}
+    if ($_GET["appFilter"] == "postal") {$filter .= "31;";}
+    if ($_GET["appFilter"] == "email") {$filter .= "32;";}
+		
+		if (! $_GET["locationFilter"] == "any") {$filter .= "40;";}
+	
+		return $filter;
+	}
+	
+	
+	
+	//
+	function applyFilter($flagged, $filter, $search) {
+    // this could be done with far fewer variables but it's way easier debugging separately.
+    $firstFilter = "";
+    $secondFilter = "";
+    $thirdFilter = "";
+    $fourthFilter = "";
+    $fifthFilter = "";
+
+    // Search specific fields [All/PosID/Title].
+    if (! strpos($filter, "00;") == FALSE) {
+        $firstFilter = $flagged;
+    } else {
+			// these should be using if ($_GET["fieldFilter"] == any/posID/title) instead of strpos.
+			// that way i can remove the checkfilter function.
+        if (! strpos($filter, "01;") == FALSE) {$i = 0; $firstFilter = filterOne($flagged, $firstFilter, $i, $search);}
+        if (! strpos($filter, "02;") == FALSE) {$i = 1; $firstFilter = filterOne($flagged, $firstFilter, $i, $search);}
+    }
+
+    // posType [Any/Full Time/Part Time].
+    if (! strpos($filter, "10;") == FALSE) {
+        $secondFilter = $firstFilter;
+    } else {
+        if (! strpos($filter, "11;") == FALSE) {$i = "fTime"; $secondFilter = filterTwo($firstFilter, $secondFilter, $i);}
+        if (! strpos($filter, "12;") == FALSE) {$i = "pTime"; $secondFilter = filterTwo($firstFilter, $secondFilter, $i);}
+    }
+    
+    // conType [Any/Ongoing/Fixed Term].
+    if (! strpos($filter, "20;") == FALSE) {
+        $thirdFilter = $secondFilter;
+    } else {
+        if (! strpos($filter, "21;") == FALSE) {$i = "ongoing"; $thirdFilter = filterThree($secondFilter, $thirdFilter, $i);}
+        if (! strpos($filter, "22;") == FALSE) {$i = "fixedTerm"; $thirdFilter = filterThree($secondFilter, $thirdFilter, $i);}
+    }
+    
+    // appType [Any/Postal/Email].
+    if (! strpos($filter, "30;") == FALSE) {
+        $fourthFilter = $thirdFilter;
+    } else {
+        if (! strpos($filter, "31;") == FALSE) {$i = 6; $fourthFilter = filterFour($thirdFilter, $fourthFilter, $i);}
+        if (! strpos($filter, "32;") == FALSE) {$i = 7; $fourthFilter = filterFour($thirdFilter, $fourthFilter, $i);}
+    }
+    
+    // Location search.
+    if (strpos($filter, "40;") == FALSE) {
+        $fifthFilter = $fourthFilter;
+    } else {
+        $i = $_GET["locationFilter"];
+        $fifthFilter = filterFive($fourthFilter, $fifthFilter, $i);
+    }
+
+    return $fifthFilter;
+	}
+	
+	
+	
+	//
+	function filterOne($flagged, $firstFilter, $i, $search) {
+		$filterJobArray = explode("\n", $flagged);
+    foreach ($filterJobArray as $filterJob) {
+			$filterFieldArray = explode("\t", $filterJob);
+			if (! $filterFieldArray[$i] == NULL) {
+				if (! stripos($filterFieldArray[$i], $search) == FALSE) {
+					$firstFilter .= "$filterJob\n";
+				}
+			}
+    }
+    return $firstFilter;
+	}
+
+	function filterTwo($firstFilter, $secondFilter, $i) {
+    $filterJobArray = explode("\n", $firstFilter);
+    foreach ($filterJobArray as $filterJob) {
+			$filterFieldArray = explode("\n", $filterJob);
+			if (! $filterFieldArray[0] == NULL) {
+				if ($filterFieldArray[4] == $i) {
+					$secondFilter .= "$filterJob\n";
+				}
+			}
+    }
+    return $secondFilter;
+	}
+
+	function filterThree($secondFilter, $thirdFilter, $i) {
+    $filterJobArray = explode("\n", $secondFilter);
+    foreach ($filterJobArray as $filterJob) {
+			$filterFieldArray = explode("\n", $filterJob);
+			if (! $filterFieldArray[0] == NULL) {
+				if ($filterFieldArray[5] == $i) {
+					$thirdFilter .= "$filterJob\n";
+				}
+			}
+    }
+    return $thirdFilter;
+	}
+
+	function filterFour($thirdFilter, $fourthFilter, $i) {
+    $filterJobArray = explode("\n", $thirdFilter);
+    foreach ($filterJobArray as $filterJob) {
+			$filterFieldArray = explode("\n", $filterJob);
+			if (! $filterFieldArray[0] == NULL) {
+				if ($filterFieldArray[$i] == TRUE) {
+					$fourthFilter .= "$filterJob\n";
+				}
+			}
+    }
+    return $fourthFilter;
+	}
+
+	function filterFive($fourthFilter, $fifthFilter, $i) {
+    $filterJobArray = explode("\n", $fourthFilter);
+    foreach ($filterJobArray as $filterJob) {
+			$filterFieldArray = explode("\n", $filterJob);
+			if (! $filterFieldArray[0] == NULL) {
+				if ($filterFieldArray[8] == $i) {
+					$fifthFilter .= "$filterJob\n";
+				}
+			}
+    }
+    return $fifthFilter;
+	}
+	
+	
+	// Print all results in a table.
   function printFlagged($flagged) {
 		$flagJobArray = explode("\n", $flagged);
 		echo "<p>Your search matched " .(count($flagJobArray)-1) ." listing";
